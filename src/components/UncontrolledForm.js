25f669fc8786f450fb7ea4625f05d8a8293156b6
zohaib166 <zohaib166@gmail.com>
2022-07-27 07:50:58 +0000

    uncontrolled form

--- src/components/UncontrolledForm.js
+++ src/components/UncontrolledForm.js
@@ -1,3 +1,10 @@
+// The uncontrolled input is similar to the traditional HTML form inputs. 
+// The DOM itself handles the form data. Here, the HTML elements maintain their
+// own state that will be updated when the input value changes. To write an 
+// uncontrolled component, you need to use a ref to get form values from the DOM.
+// In other words, there is no need to write an event handler for every state update.
+// You can use a ref to access the input field value of the form from the DOM.
+
 import React, { Component } from 'react'
 
 class UncontrolledForm extends Component {
